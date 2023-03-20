505eb1a2518e3cc8a1a8e47129101b19c4389ea5
Eugene <goldmikki@gmail.com>
2023-03-20 08:52:24 +0200

    Footer fix on mob device

--- layout/footer.php
+++ layout/footer.php
@@ -1,12 +1,12 @@
 	<footer class="footer bg-light">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-6">
+				<div class="col-6 col-md-6">
 					<p>
 						<a href="tel:<?= get_option('mytheme_phone_number') ?>" class="no-style"><?= get_option('mytheme_phone_number') ?></a>
 					</p>
 				</div>
-				<div class="col-md-6 text-end">
+				<div class="col-6 col-md-6 text-end">
 					<p>&copy; <?= date("Y") ?> <?= bloginfo("name") ?></p>
 				</div>
 			</div>
